97fb1b714b2091b6916f7586d64e34f5af3c7dc6
Mikko Lehtinen <mikko.ml.lehtinen@gmail.com>
2022-10-29 16:30:40 +0300

    EF code first database created

---
+++ Core_NET6/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Core_NET6.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace Core_NET6.Data
+{
+    // Tämä luokka pitää periyttää DB Contextista, joka on Entity Framwork Coren sisällä
+    public class ApplicationDbContext : DbContext
+    {
+        // constructot
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)   // otetaan vastaan options ja siirretään base luokkaan, joka on DbContext
+        {
+
+        }
+        public DbSet<Movie> Movies { get; set; }   //  Tämä luo Movie taulun Movies ja sillä on neljä saraketta, jotka loimme Movie luokkaan
+    }
+}
---
+++ Core_NET6/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core_NET6.Models
+{
+    public class Movie
+    {
+        [Key]  // primary key
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Rating { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
